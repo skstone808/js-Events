18a9d7c0cb2a0eb879e4c98c9069d6362cac134b
Keola Stone <skstone808@gmail.com>
2017-03-28 20:06:12 -1000

    working 2

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 
 The function will add the following content inside the paragraph with the id of `more`.
 */
-
+ 
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
 
 function moreContent(){
